5830877f3f841f1d738272de20aeeaf4a6bd0eb7
pavan ms <pavanmshsn@gmail.com>
2022-10-29 09:52:36 +0530

    first commit

---
+++ Test2.py
@@ -0,0 +1,45 @@
+import turtle
+
+from matplotlib.pyplot import fill
+ 
+board = turtle.Turtle()
+turtle.title('Pavan')
+turtle.bgcolor("green")
+
+# board.circle(30)
+# board.dot(30)
+
+board.forward(90)
+
+board.left(90)
+board.forward(180)
+
+board.left(152)
+board.forward(205)
+
+board.left(119)
+board.forward(180)
+
+board.left(115)
+board.forward(200)
+
+board.backward(200)
+
+board.left(155)
+board.forward(180)
+
+board.right(90)
+board.forward(180)
+
+board.right(90)
+board.fillcolor('blue')
+
+board.forward(180)
+
+board.right(90)
+board.forward(30)
+
+board.fillcolor('blue')
+
+
+turtle.done()
---
+++ turtle.py
@@ -0,0 +1,11 @@
+from turtle import getscreen
+
+
+from matplotlib.pyplot import fill
+
+test = turtle.getscreen()
+test.color("orange")
+test.pensize(5)
+test.shape("turtle")
+
+test.forward(100)
